d4a6b5bf9dc97e4c0e54abd742da8f4a8c6fc17e
Barbara Mayfield <bmayfield1991@gmail.com>
2020-06-07 17:16:41 -0500

    fix: moved position of a problem below the YOUR CODE HERE text

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -34,9 +34,9 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 
 # Write a function `print_tuple` that prints all the values in a tuple
+# YOUR CODE HERE
 def print_tuple(tup):
     print(tup)
-# YOUR CODE HERE
 
 
 t = (1, 2, 5, 7, 99)
